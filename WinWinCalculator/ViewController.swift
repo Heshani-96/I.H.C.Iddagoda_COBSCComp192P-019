8f7064eb84ada0f1f400580e7e57b9b66cb6868c
Heshani chamalka <you@example.com>
2021-02-05 18:49:16 +0530

    Final Commit

--- WinWinCalculator/ViewController.swift
+++ WinWinCalculator/ViewController.swift
@@ -8,10 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var buttonAC: UIButton!
+    @IBOutlet weak var buttonPlusOrMinus: UIButton!
+    @IBOutlet weak var buttonPrecentage: UIButton!
+    @IBOutlet weak var buttonDivision: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var buttonMultiplication: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var buttonSubstraction: UIButton!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var buttonAddition: UIButton!
+    @IBOutlet weak var button0: UIButton!
+    @IBOutlet weak var buttonDott: UIButton!
+    @IBOutlet weak var buttonEqual: UIButton!
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        buttonAC.layer.borderWidth = 1
+        buttonPlusOrMinus.layer.borderWidth = 1
+        buttonPrecentage.layer.borderWidth = 1
+        buttonDivision.layer.borderWidth = 1
+        button7.layer.borderWidth = 1
+        button8.layer.borderWidth = 1
+        button9.layer.borderWidth = 1
+        button4.layer.borderWidth = 1
+        buttonMultiplication.layer.borderWidth = 1
+        button5.layer.borderWidth = 1
+        button6.layer.borderWidth = 1
+        buttonSubstraction.layer.borderWidth = 1
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        buttonAddition.layer.borderWidth = 1
+        button0.layer.borderWidth = 1
+        buttonDott.layer.borderWidth = 1
+        buttonEqual.layer.borderWidth = 1
+        
+        
     }
 
 
